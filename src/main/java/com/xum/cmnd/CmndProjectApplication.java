51771e5b4f56bfced2c647782e157928c4145497
xum <xum1281517532@163.com>
2018-12-08 15:14:09 +0800

    init cmnd project

---
+++ src/main/java/com/xum/cmnd/CmndProjectApplication.java
@@ -0,0 +1,12 @@
+package com.xum.cmnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmndProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmndProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/xum/cmnd/CmndProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xum.cmnd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CmndProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
