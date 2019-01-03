a78b149b152a41985c408567c8211f308fda9f24
xum <xum1281517532@163.com>
2019-01-03 21:02:03 +0800

    Integeration
    review springboot mybatis config

--- src/main/java/com/xum/cmnd/dao/SettingMapper.java
+++ src/main/java/com/xum/cmnd/dao/SettingMapper.java
@@ -1,5 +1,8 @@
 package com.xum.cmnd.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import com.xum.cmnd.pojo.Setting;
 import com.xum.cmnd.pojo.SettingWithBLOBs;
 
@@ -9,8 +12,12 @@ public interface SettingMapper {
     int insert(SettingWithBLOBs record);
 
     int insertSelective(SettingWithBLOBs record);
+    
+    int selectAllCount();
 
     SettingWithBLOBs selectByPrimaryKey(Integer id);
+    
+    List<Map<String, Object>> selectSettingWithBootGrid(Map<String, Object> mapPara);
 
     int updateByPrimaryKeySelective(SettingWithBLOBs record);
 
---
+++ src/main/java/com/xum/cmnd/service/SettingService.java
@@ -0,0 +1,78 @@
+package com.xum.cmnd.service;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+import com.xum.cmnd.dao.SettingMapper;
+
+import com.xum.cmnd.pojo.SettingWithBLOBs;
+
+@Service(value = "settingService")
+public class SettingService {
+
+	private static final Logger LOG = LogManager.getLogger(SettingService.class);
+	
+	@Resource
+	private SettingMapper settingMapper;
+	
+	public int deleteByPrimaryKey(Integer id) {
+		int affectRow = -1;
+		affectRow = this.settingMapper.deleteByPrimaryKey(id);
+		return affectRow;
+	}
+		
+	public int insert(SettingWithBLOBs record) {
+		int affectRow = -1;
+		affectRow = this.settingMapper.insert(record);
+		return affectRow;
+	}
+	
+	public int insertSelective(SettingWithBLOBs record) {
+		int affectRow = -1;
+		affectRow = this.settingMapper.insertSelective(record);
+		return affectRow;
+	}
+	
+	public int selectAllCount() {
+		int allcount = 0;
+		allcount = this.settingMapper.selectAllCount();
+		return allcount;
+	}
+	
+	public SettingWithBLOBs selectByPrimaryKey(Integer id) {
+		SettingWithBLOBs settingWithBLOBs = null;
+		settingWithBLOBs = this.settingMapper.selectByPrimaryKey(id);
+		return settingWithBLOBs;
+	}
+	
+	public int updateByPrimaryKeySelective(SettingWithBLOBs record) {
+		int affectRow = -1;
+		affectRow = this.settingMapper.updateByPrimaryKeySelective(record);
+		return affectRow;
+	}
+	
+	public int updateByPrimaryKeyWithBLOBs(SettingWithBLOBs record) {
+		int affectRow = -1;
+		affectRow = this.settingMapper.updateByPrimaryKeyWithBLOBs(record);
+		return affectRow;
+	}
+	
+	public int updateByPrimaryKey(SettingWithBLOBs record) {
+		int affectRow = -1;
+		affectRow = this.settingMapper.updateByPrimaryKey(record);
+		return affectRow;
+	}
+	
+	public List<Map<String, Object>> selectSettingWithBootGrid(Map<String, Object> mapPara) {
+		List<Map<String, Object>> setting = null;
+		setting = this.settingMapper.selectSettingWithBootGrid(mapPara);
+		return setting;
+	}
+	
+}
