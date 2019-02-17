328320b7eb8b3a00269d75e89083feabf0fed0ff
xum <xum1281517532@163.com>
2019-02-17 18:32:18 +0800

    Study SpringBoot zookeeper
    further study it

---
+++ src/main/java/com/xum/cmnd/config/ZkConfiguration.java
@@ -0,0 +1,40 @@
+package com.xum.cmnd.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ConfigurationProperties(prefix = "zookeeper")
+@PropertySource("classpath:config/zookeeperConfig.properties")
+public class ZkConfiguration {
+
+	 @Value("${zookeeper.server}")
+	 private String zookeeperServer;
+	 
+	 @Value(("${zookeeper.sessionTimeoutMs}"))
+	 private int sessionTimeoutMs;
+	 
+	 @Value("${zookeeper.connectionTimeoutMs}")
+	 private int connectionTimeoutMs;
+	 
+	 @Value("${zookeeper.maxRetries}")
+	 private int maxRetries;
+	 
+	 @Value("${zookeeper.baseSleepTimeMs}")
+	 private int baseSleepTimeMs;
+
+	@Bean(initMethod = "init", destroyMethod = "stop")
+	public ZkClient zkClient() {
+		ZkClient zkClient = new ZkClient();
+		zkClient.setZookeeperServer(zookeeperServer);
+	    zkClient.setSessionTimeoutMs(sessionTimeoutMs);
+	    zkClient.setConnectionTimeoutMs(connectionTimeoutMs);
+	    zkClient.setMaxRetries(maxRetries);
+	    zkClient.setBaseSleepTimeMs(baseSleepTimeMs);
+	    return zkClient;
+	}
+	
+}
