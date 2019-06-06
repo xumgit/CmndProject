254209abbb82849caf872c283d6de79fc37b3cda
xum <xum1281517532@163.com>
2019-06-06 21:45:25 +0800

    study cypress
    file upload plugin, need study another plugin

--- src/main/resources/static/cyPressProject/project/cypress/support/commands.js
+++ src/main/resources/static/cyPressProject/project/cypress/support/commands.js
@@ -23,3 +23,6 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+// file-upload plugin
+import "cypress-file-upload"
